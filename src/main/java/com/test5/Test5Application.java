4c5fbad4ae1bb74b8d51805def20bb989a597dc6
prashant-tech75 <prashant.raj.sahu75@gmail.com>
2025-07-20 15:25:02 +0530

    Update Test5Application.java
    Second Commit

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		System.ut.println("Hello Prashant...")
 		SpringApplication.run(Test5Application.class, args);
 	}
 
